6da9d4a7adba316819a3a3796ecd5ad758b08225
helenhao888 <haojinzhao@yahoo.com>
2019-06-04 12:10:35 -0500

    adjust the layout and fix more link tag

--- assets/javascript/api.js
+++ assets/javascript/api.js
@@ -5,20 +5,27 @@ var animalDupFlg=false;
 var topicName="";
 var offsetNum=0;
 const limitNum=10;
-//Favorite Array is an object array. The structure of object includes title, rating and image url
+//Favorite Array is an object array. The structure of object includes title, rating,image url,still image url and animate image url
 var favoriteArr=[];
 var images=[];
-// var images={title:"",rating:"",url:"",stillImage:"",animateImage:""};
+var titleFav,ratingFav,imgFav,stillFav,animateFav;
 
 $(document).ready(function(){
 
-    initialFun();
+initialFun();
 
 function initialFun(){    
+    //initialize variables
+    topicName="";
+    offsetNum=0;
     //create initial buttons from array topics
     topics.forEach(createBtn);  
-    //get the favorites from localstorage and store in favorite Array
-    favoriteArr=JSON.parse(localStorage.getItem("favorite"));
+
+    //get the favorites from localstorage and store in favorite Array    
+    if (localStorage.getItem("favorite")!= null){
+        favoriteArr=JSON.parse(localStorage.getItem("favorite"));
+    }    
+    
 }
 
 //create  button from the each value in array topics
@@ -37,52 +44,31 @@ $(document).on("click", ".btnClass",displayFunction);
 
 //Based on the click button, call the getImgFunction process
 function displayFunction(){   
-    $(".main").empty();
-    console.log("click new", $(this).text());
+
+    $(".main").empty();  
     topicName=$(this).text();
     getImgFunction(topicName);
-
-    //create More link to allow user to get more images
-    var moreLink=$("<a>").text("More"); 
-    // moreLink.attr({'href':'#','onclick':'function(){getImgFunction(topicName);return false}','id':'more'});
-    // moreLink.attr({'href':'#','onclick':'getImgFunction(topicName)','id':'more'});
-    // moreLink.attr('onclick','getImgFunction(topicName)');
-    moreLink.attr('href','#');
-    moreLink.attr('id','more');
-    $(".main").append(moreLink);  
 
 }
 
 
-
 //API call to get the images and other information about the topic and display them
 function getImgFunction(inputName){
-
-    console.log("call image func",inputName);
-    var queryUrl="https://api.giphy.com/v1/gifs/search?q="+inputName+"&api_key=UbE45OaEiLjCyYNkPzfusZ5qgMoEAKOF&limit="+limitNum+"&offset="+offsetNum;
-   
-
+    
+    var queryUrl="https://api.giphy.com/v1/gifs/search?q="+inputName+"&api_key=UbE45OaEiLjCyYNkPzfusZ5qgMoEAKOF&limit="+limitNum+"&offset="+offsetNum;   
+// use AJAX to get the data from GIPHY 
     $.ajax({
         url: queryUrl,
         method: "GET"
     })
     .then(function(response){
       
-         console.log("data",response.data);
         var results=response.data;
 
         //from the resonse data, create image tag and other information's tag 
-       
-
         for(var i=0;i<results.length;i++){
-            // var title= results[i].title;
-            // var rating=results[i].rating;
-            // var url= results[i].images.fixed_height_small_still.url;
-            // var stillImage=results[i].images.fixed_height_small_still.url;
-            // var animateImage=results[i].images.fixed_height_small.url;            
-            
-            
-            var divBox=$("<div>").addClass("imgBox");
+
+            var divBox=$("<div>").addClass("imgBox col-lg-3 col-md-3 col-sm-4 col-4");
             var titleDiv=$("<div>").text("Title: " + results[i].title);
             titleDiv.addClass("title");
             var msgDiv=$("<div>").text("Rating: "+ results[i].rating);
@@ -97,15 +83,27 @@ function getImgFunction(inputName){
             favoriteDiv.attr("fav-status","no");
             divBox.append(titleDiv,msgDiv,favoriteDiv,imgDiv);
             $(".main").append(divBox);
-            // offsetNum++;
-            // console.log("offset",offsetNum);
+            offsetNum++;
+           
         }        
-
+        //create More link to allow user to get more images
+        var moreLink=$("<a>").text("More"); 
+        moreLink.attr('href','#');
+        moreLink.attr('id','more');
+        $(".main").append(moreLink);  
     });
 }
 
-$(document).on("click","#more",getImgFunction(topicName));
+//When user click the more link tag , call function getImgMore to call get more images
+$(document).on("click","#more",getImgMore);
 
+//Call the getImgFunction with the topicName to get more images with the same topic
+function getImgMore(){
+   
+    $("#more").remove();
+    getImgFunction(topicName);
+
+}
 
 //When the subbmit button is clicked, add new button for the input animal
  $("#addSubmit").on("click",function(event){
@@ -126,15 +124,17 @@ $(document).on("click","#more",getImgFunction(topicName));
             $(".buttonsBox").append(newBtn);            
             extTopics.push(newAnimal);
         }
-
+        //empty the addAnimal text
         $("#addAnimal").val("");
     } else{
         alert ("empty input");
     }
 })  
 
+//when image is clicked , call imgSwitchFunc to switch still and animate image
 $(document).on("click",".img",imgSwitchFunc);
 
+//when user clicks on image, switch it from still to animate or animate to still image
 function imgSwitchFunc(){
    
     if($(this).attr("imageState")==="still"){
@@ -147,96 +147,79 @@ function imgSwitchFunc(){
 
 }
 
+//when addFavorite is clicked, call function favFunction
 $(document).on("click",".addFavorite",favFunction);
 
+//add the favorite image's info to local Storage or delete it from local strorage
 function favFunction(){
-    console.log("this",$(this));
-    console.log("fav status",$(this).attr("fav-status"));
+    
     var favFlg=$(this).attr("fav-status");
-    var favbt=$(this);
+    var favorites=$(this);
+    
+    titleFav=favorites.siblings(".title").text();
+    ratingFav=favorites.siblings(".rating").text();
+    imgFav=favorites.siblings(".img").attr("src");
+    stillFav=favorites.siblings(".img").attr("data-still");
+    animateFav=favorites.siblings(".img").attr("data-animate");
+
     if(favFlg==="yes"){
-        console.log("del fav");
-        deleteFavFunction(favbt);
+        //when the favorite tag is unselected, call the delete favorite function
+        deleteFavFunction();
+        favorites.attr("fav-status","no");
+        favorites.css("color","white");
     }else{
+         // add the favorite picture to favoriteArr
+        favoriteArr.push({title:titleFav,rating:ratingFav,url:imgFav,still:stillFav,animate:animateFav});    
+        //change favorite tag's status and color
+        favorites.attr("fav-status","yes");
+        favorites.css("color","red");
+        //save the favorite array to the local storage
+        localStorage.setItem("favorite",JSON.stringify(favoriteArr));
+    }
+}
+
+//delete  favorite image process
+function deleteFavFunction(){
+
+     //If local storage has the data with key 'favorite', check if it exists, delete it 
+     if (localStorage.getItem("favorite")!= null){
+        //get the favorites from localstorage and store in favorite Array    
+        favoriteArr=JSON.parse(localStorage.getItem("favorite"));   
         
-        console.log("add fav");
-        addFavFunction(favbt);
+        var indexFav=favoriteArr.findIndex(x => x.title===titleFav && x.still===stillFav);   
+        if (indexFav!=-1)   {
+          //delete this picture from favorite array 
+           favoriteArr.splice(indexFav,1);           
+           //update local storage 
+           localStorage.setItem("favorite",JSON.stringify(favoriteArr));
+        }      
     }
-}
-
-function addFavFunction(favorites){
-    var titleFav=favorites.siblings(".title").text();
-    var ratingFav=favorites.siblings(".rating").text();
-    var imgFav=favorites.siblings(".img").attr("src");
-    var stillFav=favorites.siblings(".img").attr("data-still");
-    var animateFav=favorites.siblings(".img").attr("data-animate");
-
-    console.log("title",titleFav);
-    console.log("rate",ratingFav);
-    console.log("farr",favoriteArr);
-    // add the favoritee picture to favoriteArr
-    favoriteArr.push({title:titleFav,rating:ratingFav,url:imgFav,still:stillFav,animate:animateFav});    
-
-    favorites.attr("fav-status","yes");
-    favorites.css("color","red");
-
-    localStorage.setItem("favorite",JSON.stringify(favoriteArr));
-}
-
-function deleteFavFunction(favorites){
-
-    favorites.attr("fav-status","no");
-    favorites.css("color","white");
 
 }
 
-
+//when click on my Favorite, get them from local storage and display them
 $(".myFavorite").on("click",function(){
-    console.log("my favorite");
-    var getFav=JSON.parse(localStorage.getItem("favorite"));
-    console.log("getfav",getFav);
+    
     $(".main").empty();
-    // createImgBoxFunc(getFav);
-    for (var j=0;j<getFav.length;j++){
-        var divBox=$("<div>").addClass("imgBox");
-        var titleDiv=$("<div>").text("Title: " + getFav[j].title);
-        titleDiv.addClass("title");
-        var msgDiv=$("<div>").text("Rating: "+ getFav[j].rating);
-        msgDiv.addClass("rating");
-        var imgStillUrl=getFav[j].still;
-        var imgAnimateUrl=getFav[j].animate;
-        var imgDiv=$("<img>");
-        imgDiv.attr({src:imgStillUrl,imageState:"still"});
-        imgDiv.attr("data-animate",imgAnimateUrl);
-        imgDiv.attr("data-still",imgStillUrl).addClass("img");        
-        divBox.append(titleDiv,msgDiv,imgDiv);
-        $(".main").append(divBox);
-    }
+    var getFav=JSON.parse(localStorage.getItem("favorite"));  
+    //if can get the data from local storage, display all the favorite images
+    if (getFav != null){
+        for (var j=0;j<getFav.length;j++){
+            //create the image, title, rating tags
+            var divBox=$("<div>").addClass("imgBox col-lg-3 col-md-3 col-sm-4 col-4");
+            var titleDiv=$("<div>").text("Title: " + getFav[j].title).addClass("title");
+            var msgDiv=$("<div>").text("Rating: "+ getFav[j].rating).addClass("rating");
+            var imgStillUrl=getFav[j].still;
+            var imgAnimateUrl=getFav[j].animate;
+            var imgDiv=$("<img>");
+            imgDiv.attr({src:imgStillUrl,imageState:"still",'data-animate':imgAnimateUrl,'data-still':imgStillUrl});          
+            imgDiv.addClass("img");        
+            divBox.append(titleDiv,msgDiv,imgDiv);
+            $(".main").append(divBox);
+        }
+    } 
 })
 
-function createImgBoxFunc(input){
-
-    
-        
-        
-        var divBox=$("<div>").addClass("imgBox");
-        var titleDiv=$("<div>").text("Title: " + input.title);
-        titleDiv.addClass("title");
-        var msgDiv=$("<div>").text("Rating: "+ input.rating);
-        msgDiv.addClass("rating");
-        var imgStillUrl=input.stillImage;
-        var imgAnimateUrl=input.animateImage;
-        var imgDiv=$("<img>");
-        imgDiv.attr({src:imgStillUrl,imageState:"still"});
-        imgDiv.attr("data-animate",imgAnimateUrl);
-        imgDiv.attr("data-still",imgStillUrl).addClass("img");
-        var favoriteDiv=$("<i>").addClass("fa fa-heart addFavorite");
-        divBox.append(titleDiv,msgDiv,favoriteDiv,imgDiv);
-        $(".main").append(divBox);
-        // offsetNum++;
-        // console.log("offset",offsetNum);
-    
-}
 
 //Check duplicate topics process
 function checkDupBtn(animal){
